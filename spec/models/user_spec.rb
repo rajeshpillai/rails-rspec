49cfe29ec08f6e48bc5ed0bbe523748c286fc76b
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-08-07 16:51:52 +0530

    Failing test and .rspec format config

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation tests' do 
+    it 'ensures first name presence' do 
+      user = User.new(last_name: 'Last', email: 'test@example.com').save
+      expect(user).to eq(false)
+    end
+
+    it 'ensures last name presence' do 
+      user = User.new(first_name: 'First', email: 'test@sample.com').save 
+      expect(user).to eq(false)
+    end
+
+    it 'ensures email presence' do 
+      user = User.new(first_name: 'First', last_name: 'Last').save 
+      expect(user).to eq(false)
+    end
+
+    it 'should save successfully' do
+      user = User.new(first_name: 'First', last_name: 'Last', email: 'test@sample.com').save
+      expect(user).to eq(true)
+
+    end
+  end
+  context 'scope tests' do 
+  end
 end
